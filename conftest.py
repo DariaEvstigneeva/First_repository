0a259656bc1329eecb4f73504c6c3805e7869b04
devstigneeva <dar.evst@yandex.ru>
2022-12-09 19:38:32 +0300

    добавлены комменты

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ from fixture.application import Application
 
 #фикстура
 @pytest.fixture()
+# при запуске всех тестов, чтобы все тесты выполнялись в одном браузер в скобки вписать scope="session"
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
