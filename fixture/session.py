4c17b031911f5da2f6090f1ac071b570e799daf3
devstigneeva <dar.evst@yandex.ru>
2022-12-09 18:38:41 +0300

    незначительные изменения

--- fixture/session.py
+++ fixture/session.py
@@ -9,10 +9,8 @@ class WorkSession:
     def login(self, username, password):
         self.app.driver.find_element(By.ID, "username").send_keys(username)
         self.app.driver.find_element(By.ID, "password").send_keys(password)
-        element = self.app.driver.find_element(By.CSS_SELECTOR, ".styles__button__2b5Jq")
         self.app.driver.find_element(By.CSS_SELECTOR, ".styles__button__2b5Jq").click()
 
     # выход из системы
     def logout(self):
-        element = self.app.driver.find_element(By.CSS_SELECTOR, "body")
         self.app.driver.find_element(By.CSS_SELECTOR, ".user-menu-module__user-info-role-button__3I6l_").click()
