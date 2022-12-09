f42246779bb9369c001de95140b2d5fe5f574544
devstigneeva <dar.evst@yandex.ru>
2022-12-09 20:17:41 +0300

    изменения в названии функции

--- tests/web_091222_delete_TA.py
+++ tests/web_091222_delete_TA.py
@@ -6,7 +6,6 @@ def test1_delete_last_audience(app):
     app.link_acrm()
     app.session.login("devstigneeva", "fWd9iGZz")
     time.sleep(2)
-    #вносим изменения тут
     app.audience.delete_last_audience()
     time.sleep(2)
     app.open_sidebar()
