e0b7c4ed5a6449b14017a95439f033bb6ab65797
devstigneeva <dar.evst@yandex.ru>
2022-12-15 17:36:41 +0300

    /

--- tests/web_change_TA.py
+++ tests/web_change_TA.py
@@ -4,7 +4,7 @@ import time
 # тест на изменение последней ца
 def test1_change_last_audience(app):
     time.sleep(1)
-    app.audience.change_last_audience("Тест 13_12_2022", "Описание1")
+    app.audience.change_last_audience("Тест 15_12_2022", "Описание1")
     time.sleep(1)
 
 
@@ -12,5 +12,5 @@ def test1_change_last_audience(app):
 # тест на изменение последней ца 2
 def test2_change_last_audience(app):
     time.sleep(1)
-    app.audience.change_last_audience2("Тест 13 12 2022", None)
+    app.audience.change_last_audience2("Тест 15 12 2022", None)
     time.sleep(1)
