f78f1e62cdf84d2ccb8acadb4e5aab59a06d5b3f
devstigneeva <dar.evst@yandex.ru>
2023-04-11 18:34:26 +0300

    /

--- Инициатор_права на просмотр и копирование.py
+++ Инициатор_права на просмотр и копирование.py
@@ -5,9 +5,9 @@ HOST = "armcrm-dev.test.gosuslugi.ru"
 URL_audiences_id = "https://{HOST}/api/audiences/2131/"
 URL_audiences_copy = "https://{HOST}/api/audiences/2131/clone/"
 URL_audiences = "https://{HOST}/api/audiences/"
-URL_templates_id = "https://{HOST}/api/audiences/376/"
-URL_templates_copy = "https://{HOST}/api/audiences/376/clone/"
-URL_templates = "https://{HOST}/api/audiences/"
+URL_templates_id = "https://{HOST}/api/templates/email/376/"
+URL_templates_copy = "https://{HOST}/api/templates/email/376/clone/"
+URL_templates = "https://{HOST}/api/templates/"
 token = "eyJ2ZXIiOjEsInR5cCI6IkpXVCIsInNidCI6ImFjY2VzcyIsImFsZyI6IlJTMjU2In0.eyJuYmYiOjE2ODEyMjQ2ODgsInNjb3BlIjoiaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL29yZ19lbXBzP29yZ19vaWQ9MTAwMDMwMDY0OCZtb2RlPXcgaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL3Vzcl9pbmY_bW9kZT13Jm9pZD0yMDc5NTUyOTY0IGh0dHA6XC9cL2VzaWEuZ29zdXNsdWdpLnJ1XC9vcmdfaW5mP29yZ19vaWQ9MTAwMDMwMDY0OCZtb2RlPXcgaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL29yZ19wcm9maWxlP21vZGU9dyZvcmdfb2lkPTEwMDAzMDA2NDggaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL29yZ192aGxzP29yZ19vaWQ9MTAwMDMwMDY0OCZtb2RlPXcgaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL29yZ19pbnZ0cz9vcmdfb2lkPTEwMDAzMDA2NDgmbW9kZT13IGh0dHA6XC9cL2VzaWEuZ29zdXNsdWdpLnJ1XC9vcmdfY3R0cz9vcmdfb2lkPTEwMDAzMDA2NDgmbW9kZT13IiwiaXNzIjoiaHR0cDpcL1wvZXNpYS1kZW1vLnRlc3QuZ29zdXNsdWdpLnJ1XC8iLCJ1cm46ZXNpYTpzaWQiOiI5Y2Q4YTRkMy1mZjc2LTNlMWMtMDVhYi05ODFkODY0YmNhNmYiLCJ1cm46ZXNpYTpzYmpfaWQiOjIwNzk1NTI5NjQsImV4cCI6MTY4MTIyODI4OCwiaWF0IjoxNjgxMjI0Njg4LCJjbGllbnRfaWQiOiJQR1UifQ.kFI3Xrx0u6fSxOz1BMDy5LkEbr5uU2S8pUmbE8SH86RJJbld9DqpFURXdQHntIBAtuN215dDqRqUkfoZTUClcNRiqZ3a1TwWbOchST01MwssV7KSAQfOtVuXTKbco6nWw8lVgQeoaSgyRVRLy3giyFYN4-Qx5lDJGrH1I3847ctNsB5pIFN1eACaayN_4xhFza3vO0iriLV7vbzfYsStsmGFDbu6temk9vLOJyYd8HrK5Lad9VIyrs2994JecNS5g3SkUAaLNebaHotBPHr2OaH82dlGl3VPPQ7hwgq1QtsL2W0UgyE2yMXf6SoloB34W33e0NwQGlS7fBLS21u7AQ"
 
 # успешное получение ца
@@ -90,7 +90,7 @@ print("")
 response = requests.get(
     URL_templates.format(HOST=HOST),
      headers={"Authorization":f"Bearer {token}"},
-     params={"search":f"2124"},
+     params={"search":f"376"},
 )
 pprint(response.json())
 
--- МСЭ_Создание ЦА.py
+++ МСЭ_Создание ЦА.py
@@ -3,10 +3,12 @@ from pprint import pprint
 
 HOST = "armcrm-dev.test.gosuslugi.ru"
 URL_audiences = "https://{HOST}/api/audiences/"
+URL_templates = "https://{HOST}/api/templates/email/"
 token = "eyJ2ZXIiOjEsInR5cCI6IkpXVCIsInNidCI6ImFjY2VzcyIsImFsZyI6IlJTMjU2In0.eyJuYmYiOjE2ODEyMjQ4NTQsInNjb3BlIjoiaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL29yZ19pbmY_b3JnX29pZD0yMDc5NTUxNzk1Jm1vZGU9dyBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvb3JnX2VtcHM_b3JnX29pZD0yMDc5NTUxNzk1Jm1vZGU9dyBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvb3JnX3Byb2ZpbGU_bW9kZT13Jm9yZ19vaWQ9MjA3OTU1MTc5NSBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvb3JnX2N0dHM_b3JnX29pZD0yMDc5NTUxNzk1Jm1vZGU9dyBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvb3JnX2ludnRzP29yZ19vaWQ9MjA3OTU1MTc5NSZtb2RlPXcgaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL29yZ192aGxzP29yZ19vaWQ9MjA3OTU1MTc5NSZtb2RlPXcgaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL3Vzcl9pbmY_bW9kZT13Jm9pZD0xMDAwMjk5MjgyIiwiaXNzIjoiaHR0cDpcL1wvZXNpYS1kZW1vLnRlc3QuZ29zdXNsdWdpLnJ1XC8iLCJ1cm46ZXNpYTpzaWQiOiI2Y2U1NTQ3NC1mZmM5LTYzYTYtNWE2Yi1kNGJmY2IzYzBlZTUiLCJ1cm46ZXNpYTpzYmpfaWQiOjEwMDAyOTkyODIsImV4cCI6MTY4MTIyODQ1NCwiaWF0IjoxNjgxMjI0ODU0LCJjbGllbnRfaWQiOiJQR1UifQ.peKChf4TFN55__je_4fdoyz0XuUeA3nKvc_0Zo3Uor4LsFhFgG7OqST_rv1rqBFoSHvxBwORwYolYadbp7CjynnjXnMS2py_jpw3J9OokaJIUZIseBoeIagkhYwRds7F2yxdFUTrMVpY2Kpa1bYCT7uCnS0XAmwP2LIKE0keXY22qK7c2BFKkPNj13CmPqdQi1lp4s1Y41iHTOUQUuw8Ub8eI65ZmKxgRH2jgW4TheXHhNxK9Kjgsgi90vH61igf867RoIMv5LWyuTsZYDJvleEVF29z8BKNXbIm6wI5XaU7FsFS2RSlSn9jIbyMF8xFFsGf1Hfm0ZMGtlZrnYA9tA"
 
+# успешное создание ца
 body = {
-    "name": "МСЭ ЧА",
+    "name": "МСЭ ЧА4",
     "category": "PHYSICAL"
 }
 
@@ -16,8 +18,19 @@ response = requests.post(
     data=body,
 )
 
-try:
-    pprint(response.json())
-except Exception:
-        print("Нет json у ответа")
-        pprint(response)
+pprint(response.json())
+
+print("")
+
+# создание шаблона недоступно
+body = {
+    "name": "МСЭ ЧА2"
+}
+
+response = requests.post(
+    URL_templates.format(HOST=HOST),
+    headers={"Authorization": f"Bearer {token}"},
+    data=body,
+)
+
+pprint(response.json())
