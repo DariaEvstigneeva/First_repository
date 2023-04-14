f90cf4d9b826fa4c629ff6af9559190652d48fb9
devstigneeva <dar.evst@yandex.ru>
2023-04-14 11:58:45 +0300

    /

--- ЦА УДАЛЕНА.py
+++ ЦА УДАЛЕНА.py
@@ -2,36 +2,51 @@ import requests
 from pprint import pprint
 
 HOST = "armcrm-dev.test.gosuslugi.ru"
-URL_audiences = "https://{HOST}/api/audiences/"
-URL_templates = "https://{HOST}/api/templates/email/"
+ID_audience = "2142"
+audiences_updated_at = "2023-04-14T11:17:15.386857+03:00"
+URL_audiences_update_status = "https://{HOST}/api/audiences/{ID_audience}/update-status/"
+
 token = "eyJ2ZXIiOjEsInR5cCI6IkpXVCIsInNidCI6ImFjY2VzcyIsImFsZyI6IlJTMjU2In0.eyJuYmYiOjE2ODE0NTk1NzAsInNjb3BlIjoiaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL29yZ19pbmY_b3JnX29pZD0yMDc5NTUxNzk1Jm1vZGU9dyBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvb3JnX2VtcHM_b3JnX29pZD0yMDc5NTUxNzk1Jm1vZGU9dyBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvb3JnX3Byb2ZpbGU_bW9kZT13Jm9yZ19vaWQ9MjA3OTU1MTc5NSBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvb3JnX2N0dHM_b3JnX29pZD0yMDc5NTUxNzk1Jm1vZGU9dyBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvb3JnX2ludnRzP29yZ19vaWQ9MjA3OTU1MTc5NSZtb2RlPXcgaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL29yZ192aGxzP29yZ19vaWQ9MjA3OTU1MTc5NSZtb2RlPXcgaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL3Vzcl9pbmY_bW9kZT13Jm9pZD0xMDAwMjk5MjgyIiwiaXNzIjoiaHR0cDpcL1wvZXNpYS1kZW1vLnRlc3QuZ29zdXNsdWdpLnJ1XC8iLCJ1cm46ZXNpYTpzaWQiOiJjM2YxNGIzNy1jMDA0LWE1NmMtNGFjNS01ZWI4MGI1YmNhNzAiLCJ1cm46ZXNpYTpzYmpfaWQiOjEwMDAyOTkyODIsImV4cCI6MTY4MTQ2MzE3MCwiaWF0IjoxNjgxNDU5NTcwLCJjbGllbnRfaWQiOiJQR1UifQ.HvPKp3oVoDs65bTlFELyHGzULXTitHGk_VC3vUyySL7hT6OtZi-aVAzd7BlOZsuablkATlmNS1pQdzlmV3g5aqI9HcTm1_tRo960FN4nITZfVdAhFv3l9dTobX9o1SSWsSmt01_adISKqOrWPsmb-iF2shS3Zg-G4_Mx9fMz_O4rZOjxxvZdJ4sjRu2j8d-hqCCe_QwTeFzmso2XxhjiMI_bkZ3JiLM_lSZtGXumJAxfIK9Chk6TmJmFBpN_fF0cKSfbpMy8L_onAVXfc9dwdta9cv0UowbwTyGae27_TTe75YxDgZ1bjkujm5WvkMgQs-yHQkZp5GY70aDRtGondg"
 
-# успешное создание ца
+# перевод ца в статус черновик ненедоступно
 body = {
-    "name": "МСЭ 3",
-    "category": "PHYSICAL"
+    "status": "DRAFT",
+    "updated_at": audiences_updated_at
 }
 
-response = requests.post(
-    URL_audiences.format(HOST=HOST),
+response = requests.patch(
+    URL_audiences_update_status.format(HOST=HOST, ID_audience=ID_audience),
     headers={"Authorization": f"Bearer {token}"},
     data=body,
 )
-
 pprint(response.json())
 
 print("")
 
-# создание шаблона недоступно
+# перевод ца в статус активна недоступно
 body = {
-    "name": "МСЭ 1",
-    "body": "МСЭ"
+    "status": "ACTIVE",
+    "updated_at": audiences_updated_at
 }
 
-response = requests.post(
-    URL_templates.format(HOST=HOST),
+response = requests.patch(
+    URL_audiences_update_status.format(HOST=HOST, ID_audience=ID_audience),
     headers={"Authorization": f"Bearer {token}"},
     data=body,
 )
+pprint(response.json())
 
+print("")
+
+# перевод ца в статус удалена недоступно
+body = {
+    "status": "DELETED",
+    "updated_at": audiences_updated_at
+}
+
+response = requests.patch(
+    URL_audiences_update_status.format(HOST=HOST, ID_audience=ID_audience),
+    headers={"Authorization": f"Bearer {token}"},
+    data=body,
+)
 pprint(response.json())
--- ШАБЛОН ЧЕРНОВИК.py
+++ ШАБЛОН ЧЕРНОВИК.py
@@ -2,36 +2,50 @@ import requests
 from pprint import pprint
 
 HOST = "armcrm-dev.test.gosuslugi.ru"
-URL_audiences = "https://{HOST}/api/audiences/"
-URL_templates = "https://{HOST}/api/templates/email/"
+
+ID_template = "381"
+URL_templates_disable = "https://{HOST}/api/templates/email/{ID_template}/disable/"
+URL_templates_deactivate = "https://{HOST}/api/templates/email/{ID_template}/deactivate/"
+URL_templates_activate = "https://{HOST}/api/templates/email/{ID_template}/activate/"
+
 token = "eyJ2ZXIiOjEsInR5cCI6IkpXVCIsInNidCI6ImFjY2VzcyIsImFsZyI6IlJTMjU2In0.eyJuYmYiOjE2ODE0NjE2ODksInNjb3BlIjoiaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL29yZ19lbXBzP29yZ19vaWQ9MTAwMDMwMDY0OCZtb2RlPXcgaHR0cDpcL1wvZXNpYS5nb3N1c2x1Z2kucnVcL29yZ19pbmY_b3JnX29pZD0xMDAwMzAwNjQ4Jm1vZGU9dyBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvb3JnX3ZobHM_b3JnX29pZD0xMDAwMzAwNjQ4Jm1vZGU9dyBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvb3JnX3Byb2ZpbGU_b3JnX29pZD0xMDAwMzAwNjQ4IGh0dHA6XC9cL2VzaWEuZ29zdXNsdWdpLnJ1XC9vcmdfaW52dHM_b3JnX29pZD0xMDAwMzAwNjQ4Jm1vZGU9dyBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvb3JnX2N0dHM_b3JnX29pZD0xMDAwMzAwNjQ4Jm1vZGU9dyBodHRwOlwvXC9lc2lhLmdvc3VzbHVnaS5ydVwvdXNyX2luZj9tb2RlPXcmb2lkPTEwMDAyOTkyODIiLCJpc3MiOiJodHRwOlwvXC9lc2lhLWRlbW8udGVzdC5nb3N1c2x1Z2kucnVcLyIsInVybjplc2lhOnNpZCI6IjRjMDgyZjkzLTJmM2QtZmVkNi0yOTdiLTU1ZmEwYjA0Y2RkZSIsInVybjplc2lhOnNial9pZCI6MTAwMDI5OTI4MiwiZXhwIjoxNjgxNDY1Mjg5LCJpYXQiOjE2ODE0NjE2ODksImNsaWVudF9pZCI6IlBHVSJ9.p3Rttv094Z6E4uztz2b7GdGypYBA33TDTL4npKRMU-S5nxwAwplndkjhMctyCcZ5rr7p-rJoB2ituMbyBH0XEjTOZ5wYXpy1LuXVlNco24fA-sX5soaZhC1MOwDeRA9eJy6mcmPeFUUN6tce6MLhOTUuRhr4-AyjwHhqkPnG471SnhLTCVVXb24f39_BuJ2Z0cdTIxzp-fIM7tKzztuHLU3IC9m8vh07-0GutSBOmjaxsf595dBao-0kwrCCYqJK8zooec3Sph2W-rtOaK5Ibc0lTlHDB2cMVhVr5WKVXfvs9cvBUhIBi-aFAAk3oEdJYNcsvzGLaJwuoRIEH_6pPQ"
 
-# успешное создание ца
+# перевод шаблона в статус черновик недоступно
 body = {
-    "name": "ММЦ ЧА",
-    "category": "PHYSICAL"
 }
 
-response = requests.post(
-    URL_audiences.format(HOST=HOST),
+response = requests.put(
+    URL_templates_deactivate.format(HOST=HOST, ID_template=ID_template),
     headers={"Authorization": f"Bearer {token}"},
     data=body,
 )
-
 pprint(response.json())
 
 print("")
 
-# успешное создание шаблона
+# перевод шаблона в статус удален недоступно
 body = {
-    "name": "ММЦ 1",
-    "body": "ММЦ"
 }
 
-response = requests.post(
-    URL_templates.format(HOST=HOST),
+response = requests.put(
+    URL_templates_disable.format(HOST=HOST, ID_template=ID_template),
     headers={"Authorization": f"Bearer {token}"},
     data=body,
 )
-
 pprint(response.json())
+
+print("")
+
+# перевод шаблона в статус активен доступно
+body = {
+}
+
+response = requests.put(
+    URL_templates_activate.format(HOST=HOST, ID_template=ID_template),
+    headers={"Authorization": f"Bearer {token}"},
+    data=body,
+)
+pprint(response)
+
+
+
